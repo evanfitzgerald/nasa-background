ecf5a6f428dab11ba08b763c91084b4a05f16ebe
Evan Fitzgerald <evandfitz@gmail.com>
2019-09-05 23:41:19 -0300

    initial commit

---
+++ potd.py
@@ -0,0 +1,42 @@
+
+import requests, shutil, subprocess, datetime
+
+# enter nasa api key
+api_key = 'enter_your_nasa_api_key_here'
+
+# enter where you want the photos to be stored
+filename = 'enter_directory_for_photos'
+
+# http request
+url = 'https://api.nasa.gov/planetary/apod?api_key=' + api_key
+response = requests.get(url)
+
+# lets use know if our http request was sucessful
+if response.status_code == 200:
+
+    # data from the request
+    data = response.text
+
+    # current date, used for naming file
+    now = datetime.datetime.now()
+    date = str(now.month) + '-' + str(now.day) + '-' + str(now.year)
+    filename = filename + date + '.jpg'
+
+    # parsing data to get url of photo
+    response = requests.get(data[data.find('https:'):data.find('.jpg')] + '.jpg', stream=True)
+
+    # downloading photo
+    photo = open(filename, 'wb')
+    response.raw.decode_content = True
+    shutil.copyfileobj(response.raw, photo)
+    del response
+
+    # changes the background image
+    SCRIPT = """osascript -e 'tell application "Finder" to set desktop picture to """ + '"' +filename+ '"' +""" as POSIX file'"""
+    subprocess.Popen(SCRIPT, shell=True)
+
+    # to make this process run during a login, see steps on using the automator in macOS
+
+# lets us know if there is an error with the http request
+else:
+    print('error with request')
